e56d76644ebcaa1c94194b4a03d5254e3e9d648c
Sebastian <sebastian.lauks13@gmail.com>
2020-10-05 16:34:32 +0200

    Add Drawer menu

---
+++ app/src/main/java/lauks/sebastian/footballacademies/view/drawer/DrawerActivity.kt
@@ -0,0 +1,42 @@
+package lauks.sebastian.footballacademies.view.drawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_drawer.*
+import lauks.sebastian.footballacademies.R
+
+class DrawerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drawer)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
