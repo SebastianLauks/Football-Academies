887afcaab674f89bb3dd4cfcc870db95e618afb3
Sebastian <sebastian.lauks13@gmail.com>
2020-10-19 19:01:21 +0200

    First version of navigation drawer

--- app/src/main/java/lauks/sebastian/footballacademies/view/drawer/DrawerActivity.kt
+++ app/src/main/java/lauks/sebastian/footballacademies/view/drawer/DrawerActivity.kt
@@ -3,12 +3,14 @@ package lauks.sebastian.footballacademies.view.drawer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_drawer.*
 import lauks.sebastian.footballacademies.R
 
-class DrawerActivity : AppCompatActivity() {
+class DrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +22,26 @@ class DrawerActivity : AppCompatActivity() {
         drawerToggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        
+        navigation_view.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    Toast.makeText(this,"Test",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    true
+                }
+
+            }
+        }
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -31,6 +52,7 @@ class DrawerActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 
     override fun onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
